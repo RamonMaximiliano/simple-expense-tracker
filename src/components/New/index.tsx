d982c457454d5e3cb79d7706e1443506ffbfb963
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-07-09 12:53:51 -0300

    continuying expense input

--- src/components/New/index.tsx
+++ src/components/New/index.tsx
@@ -2,22 +2,42 @@ import './styles.css'
 import {useState} from 'react'
 
 type Str = string
+type Num = number
+
+
+//limpar os inputs quando acionar o Adicionar
 
 function New(props:any) {
+    //Criar um state para cada field do expense input
+    const [data, setData] = useState<Str>('')
+    const [categoria, setCategoria] = useState<Str>('')
     const [titulo, setTitulo] = useState<Str>('')
-
-
-    console.log(titulo)
+    const [valor, setValor] = useState<Num>(0)
+ 
+   function newExpense(){
+        console.log(
+            {
+                date: data,
+                category: categoria,
+                title: titulo,
+                value: valor
+            }
+        )
+        setData('')
+        setCategoria('')
+        setTitulo('')
+        setValor(0)
+    } 
 
     return (
-        <form className="mainNew" onSubmit={props.onsubmitExpense}>
+        <form className="mainNew">
             <div>
                 <p>Data</p>
-                <input type="date"></input>
+                <input id="dateInput"  type="date" onChange={e => setData(String(e.target.value))}></input>
             </div>
             <div>
                 <label>Categoria</label><br></br>
-                <select>
+                <select onChange={e => setCategoria(String(e.target.value))}>
                     <option>Alimentação</option>
                     <option>Aluguel</option>
                     <option>Salário</option>
@@ -29,9 +49,9 @@ function New(props:any) {
             </div>
             <div>
                 <p>Valor</p>
-                <input type="number"></input>
+                <input type="number" onChange={e => setValor(Number(e.target.value))}></input>
             </div>
-            <input type="button" value="Adicionar"></input>
+            <input type="button" value="Adicionar" onClick={newExpense}></input>
         </form>
     )
 }
